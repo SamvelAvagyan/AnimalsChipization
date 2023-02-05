b8b48b745567212c3885276bda6c270f86201470
SamvelAvagyan <avagyansamvel07@gmail.com>
2023-02-05 16:32:31 +0400

    Add validations for account

---
+++ WebApi/Validations/AccountRegistrationViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using WebApi.ViewModels;
+
+namespace WebApi.Validations
+{
+    public class AccountRegistrationViewModelValidator : AbstractValidator<AccountRegistrationViewModel>
+    {
+        public AccountRegistrationViewModelValidator()
+        {
+            RuleFor(account => account.FirstName).NotNull().NotEmpty();
+            RuleFor(account => account.LastName).NotNull().NotEmpty();
+            RuleFor(account => account.Email).NotNull().NotEmpty();
+            RuleFor(account => account.Password).NotNull().NotEmpty().MatchEmailRules();
+        }
+    }
+}
---
+++ WebApi/Validations/CustomValidations.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Validators;
+using FluentValidation;
+using System.Net.Mail;
+
+namespace WebApi.Validations
+{
+    public static class CustomValidations
+    {
+        public static IRuleBuilderOptions<T, string> MatchEmailRules<T>(this IRuleBuilder<T, string> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new RegularExpressionValidator<T>(@"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"));
+        }
+    }
+}
